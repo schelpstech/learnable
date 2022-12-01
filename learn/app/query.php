3d4e50870d87172e306dd605dbf7442a32182276
schelpstech <sunkanmi@schelps.com.ng>
2022-12-01 21:45:36 +0100

    revrese

--- learn/app/query.php
+++ learn/app/query.php
@@ -607,6 +607,9 @@ if (isset($_SESSION['active']) && isset($_SESSION['user_type']) && $_SESSION['us
             'staffid' => $_SESSION['active'],
             'term' => $active_term['term'],
         ),
+        'joinl' => array(
+            'lhpsubject' => ' on lhpalloc.sbjid = lhpsubject.sbjid ',
+        )
     );
     $all_subject_allocated = $model->getRows($tblName, $conditions);
 
