b060dea5d800a1d5df52daf52069a0a2ebf42dc9
schelpstech <sunkanmi@schelps.com.ng>
2024-07-30 19:13:10 +0100

    backup-update

--- learn/controller/backup.class.php
+++ learn/controller/backup.class.php
@@ -1,86 +1,91 @@
 <?php
-class DBbackup
+
+class DBBackup
 {
-
-    // Refer to database connection
     private $db;
+    private $suffix;
 
-    public $suffix;
-    public $dirs;
-
-    // Instantiate object with database connection
-    public function __construct($db_conn)
+    public function __construct(PDO $db_conn)
     {
         $this->db = $db_conn;
         $this->suffix = date('Ymd_His');
     }
-    public function run_backup($tables)
+
+    public function runBackup($tables = '*')
     {
-        $output = "-- database backup - " . date('Y-m-d H:i:s') . PHP_EOL;
-        $output .= "SET NAMES utf8;" . PHP_EOL;
-        $output .= "SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';" . PHP_EOL;
-        $output .= "SET foreign_key_checks = 0;" . PHP_EOL;
-        $output .= "SET AUTOCOMMIT = 0;" . PHP_EOL;
-        $output .= "START TRANSACTION;" . PHP_EOL;
-        //get all table names
-        if ($tables == '*') {
-            $tables = [];
-            $query = $this->db->prepare('SHOW TABLES');
-            $query->execute();
-            while ($row = $query->fetch(PDO::FETCH_NUM)) {
-                $tables[] = $row[0];
-            }
-            $query->closeCursor();
+        $output = $this->generateBackupHeader();
+        $tables = $this->getTableNames($tables);
+
+        foreach ($tables as $table) {
+            $output .= $this->generateTableDump($table);
+        }
+
+        $output .= $this->generateBackupFooter();
+        $this->saveBackupToFile($output);
+    }
+
+    private function generateBackupHeader()
+    {
+        return "-- Database backup - " . date('Y-m-d H:i:s') . PHP_EOL
+             . "SET NAMES utf8;" . PHP_EOL
+             . "SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';" . PHP_EOL
+             . "SET foreign_key_checks = 0;" . PHP_EOL
+             . "SET AUTOCOMMIT = 0;" . PHP_EOL
+             . "START TRANSACTION;" . PHP_EOL;
+    }
+
+    private function getTableNames($tables)
+    {
+        if ($tables === '*') {
+            $query = $this->db->query('SHOW TABLES');
+            $tables = $query->fetchAll(PDO::FETCH_COLUMN);
         } else {
             $tables = is_array($tables) ? $tables : explode(',', $tables);
         }
+        return $tables;
+    }
 
-        foreach ($tables as $table) {
+    private function generateTableDump($table)
+    {
+        $output = "";
 
-            $query = $this->db->prepare("SELECT * FROM `$table`");
-            $query->execute();
-            $output .= "DROP TABLE IF EXISTS `$table`;" . PHP_EOL;
+        // Drop table if exists
+        $output .= "DROP TABLE IF EXISTS `$table`;" . PHP_EOL;
 
-            $query2 = $this->db->prepare("SHOW CREATE TABLE `$table`");
-            $query2->execute();
-            $row2 = $query2->fetch(PDO::FETCH_NUM);
-            $query2->closeCursor();
-            $output .= PHP_EOL . $row2[1] . ";" . PHP_EOL;
+        // Create table statement
+        $createTableQuery = $this->db->query("SHOW CREATE TABLE `$table`");
+        $createTableStmt = $createTableQuery->fetch(PDO::FETCH_COLUMN, 1);
+        $output .= PHP_EOL . $createTableStmt . ";" . PHP_EOL;
 
-            while ($row = $query->fetch(PDO::FETCH_NUM)) {
-                $output .= "INSERT INTO `$table` VALUES(";
-                for ($j = 0; $j < count($row); $j++) {
-                    $row[$j] = addslashes($row[$j]);
-                    $row[$j] = str_replace("\n", "\\n", $row[$j]);
-                    if (isset($row[$j]))
-                        $output .= "'" . $row[$j] . "'";
-                    else
-                        $output .= "''";
-                    if ($j < (count($row) - 1))
-                        $output .= ',';
-                }
-                $output .= ");" . PHP_EOL;
-            }
+        // Insert data
+        $dataQuery = $this->db->query("SELECT * FROM `$table`");
+        while ($row = $dataQuery->fetch(PDO::FETCH_ASSOC)) {
+            $values = array_map([$this, 'escapeString'], array_values($row));
+            $output .= "INSERT INTO `$table` VALUES (" . implode(',', $values) . ");" . PHP_EOL;
         }
-        $output .= PHP_EOL . PHP_EOL;
 
-        $output .= "COMMIT;";
-        //save filename
-
-        $filename = 'db_backup_' . $this->suffix.'.zip';
-        $this->writeUTF8filename($filename, $output);
-        echo '<a href="./'.$filename.'">Click to download backup file</a>';
+        return $output;
     }
 
-
-    private function writeUTF8filename($fn, $c)
-    { /* save as utf8 encoding */
-        $f = fopen($fn, "w+");
-        # Now UTF-8 - Add byte order mark
-        fwrite($f, pack("CCC", 0xef, 0xbb, 0xbf));
-        fwrite($f, $c);
-        fclose($f);
+    private function escapeString($value)
+    {
+        if ($value === null) {
+            return "NULL";
+        }
+        return "'" . addslashes($value) . "'";
     }
 
+    private function generateBackupFooter()
+    {
+        return PHP_EOL . "COMMIT;" . PHP_EOL;
+    }
+
+    private function saveBackupToFile($content)
+    {
+        $filename = 'db_backup_' . $this->suffix . '.sql';
+        file_put_contents($filename, $content);
+        echo '<a href="./' . $filename . '">Click to download backup file</a>';
+    }
 }
+
 ?>
